af66e57dcc320c15c2b3921535a117a8b66a96e7
Su <su@Sus-MacBook-Pro.local>
2022-06-26 12:50:00 +0700

    Completed Responsive Screen

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,45 +1,64 @@
-import React, {useState} from "react";
-import Logo from "../../imgs/logo.png";
+import React, { useState } from "react";
+import Logo from "../../imgs/logo.png"
+import { UilSignOutAlt } from "@iconscout/react-unicons";
 import { SidebarData } from "../../Data/Data";
-import {UilSignOutAlt} from '@iconscout/react-unicons'
+import { UilBars } from "@iconscout/react-unicons";
+import { motion } from "framer-motion";
 
-import "./SideBar.css";
+import './SideBar.css'
 
-const SideBar = () => {
+const Sidebar = () => {
+  const [selected, setSelected] = useState(0);
 
-    const [selected, setSelected] = useState(0)
+  const [expanded, setExpaned] = useState(true)
+
+  const sidebarVariants = {
+    true: {
+      left : '0'
+    },
+    false:{
+      left : '-60%'
+    }
+  }
+  console.log(window.innerWidth)
   return (
-    <div className="SideBar">
-        {/* Logo */}
-      <div className="Logo">
-        <img src={Logo} alt="" />
+    <>
+      <div className="bars" style={expanded?{left: '60%'}:{left: '5%'}} onClick={()=>setExpaned(!expanded)}>
+        <UilBars />
+      </div>
+    <motion.div className='sidebar'
+    variants={sidebarVariants}
+    animate={window.innerWidth<=768?`${expanded}`:''}
+    >
+      {/* logo */}
+      <div className="logo">
+        <img src={Logo} alt="logo" />
         <span>
-            Sh<span>o</span>ps
+          Sh<span>o</span>ps
         </span>
       </div>
 
-      {/* Menu */}
-      <div className="Menu">
-        {SidebarData.map((item, index)=>{
-            return(
-                <div className={selected===index?'menuItem active':'menuItem'}
-                key={index}
-                onClick={()=>setSelected(index)}
-                >
-                    <item.icon/>
-                    <span>
-                        {item.heading}
-                    </span>
-
-                </div>
-            )
+      <div className="menu">
+        {SidebarData.map((item, index) => {
+          return (
+            <div
+              className={selected === index ? "menuItem active" : "menuItem"}
+              key={index}
+              onClick={() => setSelected(index)}
+            >
+              <item.icon />
+              <span>{item.heading}</span>
+            </div>
+          );
         })}
+        {/* signoutIcon */}
         <div className="menuItem">
-        <UilSignOutAlt/>
+          <UilSignOutAlt />
         </div>
       </div>
-    </div>
+    </motion.div>
+    </>
   );
 };
 
-export default SideBar;
+export default Sidebar;
